539a163449d3e4f35a0c52e83ce59247dccd0732
Khoan <phungdang.khoan@gmail.com>
2023-04-01 16:22:54 +0700

    category and product

--- src/components/Category/Category.jsx
+++ src/components/Category/Category.jsx
@@ -1,10 +1,14 @@
 import "./Category.scss";
-import React from 'react';
 
-export default function Category() {
-  return (
-    <div>Category</div>
-  )
-}
+import Products from "../Products/Products"
 
+const Category = () => {
+    return <div className="category-main-content">
+        <div className="layout">
+            <div className="category-title">Category Title</div>
+            <Products innerPage={true} />
+        </div>
+    </div>;
+};
 
+export default Category;
--- src/components/Home/Category/Category.jsx
+++ src/components/Home/Category/Category.jsx
@@ -1,6 +1,30 @@
-import "./Category.scss";
-const Category = () => {
-    return <div>Category</div>;
-};
 
-export default Category;
+import "./Category.scss";
+import React from 'react';
+import cart1 from "../../../assets/category/cat-1.jpg"
+import cart2 from "../../../assets/category/cat-2.jpg"
+import cart3 from "../../../assets/category/cat-3.jpg"
+import cart4 from "../../../assets/category/cat-4.jpg"
+
+export default function Category() {
+  return (
+    <div className="shop-by-category">
+      <div className="categories">
+        <div className="category">
+            <img src={cart1} alt="" />
+        </div>
+        <div className="category">
+            <img src={cart2} alt="" />
+        </div>
+        <div className="category">
+            <img src={cart3} alt="" />
+        </div>
+        <div className="category">
+            <img src={cart4} alt="" />
+        </div>
+      </div>
+    </div>
+  )
+}
+
+
